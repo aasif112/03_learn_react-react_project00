8cc130a8df559245ea5fb8beebffead0dff8e565
Aasif Ali <156135474+aasif112@users.noreply.github.com>
2024-07-30 06:26:13 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 
 import Dinner from "./dinner";
 
-function App() {
+function Manue() {
   return    ( <div >
               <Dinner  mainDish="Chiken Biryani"  sweetDish="Kheer"  drinks="Cold-Drinks" />
               <Dinner  mainDish="Chiken Karhai"  sweetDish="Halwa"  drinks="Juice" />
@@ -12,4 +12,4 @@ function App() {
   );
 }
 
-export default App;
+export default Manue;
